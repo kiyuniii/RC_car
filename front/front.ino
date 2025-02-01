7c8b811a50b2bb597fe3812b553b4f264d9ca854
kiyuniii <bkyun3012@sju.ac.kr>
2025-02-01 20:05:00 +0900

    2025.02.01 ==== CAN PROTOCOL BEGIN ==== EDIT: simple_rx.ino  - parse data(joystick)  - determine vehicle's operation with xValue/yValue  - send can_data(xValue,yValue,opMode) EDIT: front.ino  - parse data(can_data)  - operate with can_data's opMode

--- front/front.ino
+++ front/front.ino
@@ -1,41 +1,94 @@
 #include <SoftwareSerial.h>
 #include <AFMotor.h>
+#include <mcp_can.h>
+#include <SPI.h>
 
 // #define L_MOTOR_PIN 3
 // #define R_MOTOR_PIN 4
-#define SPEED  200
+#define CAN0_INT 2          //D2
+MCP_CAN CAN0(10);           //#define MCP_CS_PIN 10
 
+#define SPEED  200
 AF_DCMotor L_Motor(3);
 AF_DCMotor R_Motor(4);
 
+#define MODE_FORWARD  1
+#define MODE_BACKWARD 2
+#define MODE_LEFT     3
+#define MODE_RIGHT    4
+#define MODE_STOP     5
+
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+char msgString[128];
+
 void setup() {
   Serial.begin(115200);
+
+  /* [MCP2515] CAN INIT */
+  if(CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_16MHZ) == CAN_OK) {
+    Serial.println("[DONE] MCP2515 Initialized");
+  } else {
+    Serial.println("[FAIL] MCP2515 Initialized");
+  }
+
+  CAN0.setMode(MCP_NORMAL); // Change to normal mode to allow messages to be transmitted
+  pinMode(CAN0_INT, INPUT);
+
   L_Motor.setSpeed(SPEED);
   R_Motor.setSpeed(SPEED);
 }
 
 void loop() {
-  move_forward(SPEED);
-  delay(1000);
-  stop();
-  delay(500);
+  int xValue, yValue;
+  uint8_t opMode;
 
-  move_backward(SPEED);
-  delay(1000);
-  stop();
-  delay(500);
+  if(!digitalRead(CAN0_INT)) {
+    CAN0.readMsgBuf(&rxId, &len, rxBuf);
 
-  turn_left(SPEED);
-  delay(1000);
-  stop();
-  delay(500);
+    if(rxId == 0x100) {     //CHECK WHETHER CAN_TX IS RF_RX
+      xValue = (rxBuf[0] << 8) | rxBuf[1];
+      yValue = (rxBuf[2] << 8) | rxBuf[3];
+      opMode = rxBuf[4];
+    }
+    
+    switch(opMode) {
+      case MODE_FORWARD:
+        Serial.println("==== FORWARD ====");
+        move_forward(speed);
+        break;
+      case MODE_BACKWARD:
+        Serial.println("==== BACKWARD ====");
+        move_backward(speed);
+        break;
+      case MODE_LEFT:
+        Serial.println("==== TURN_LEFT ====");
+        turn_left(speed);
+        break;
+      case MODE_RIGHT:
+        Serial.println("==== TURN_RIGHT ====");
+        turn_right(speed);
+        break;
+    }
 
-  turn_right(SPEED);
-  delay(1000);
-  stop();
-  delay(500);
+    //PRINT RECEIVED VALUES
+    Serial.println("=== Received CAN Data ===");
+    Serial.print("xValue: "); Serial.println(xValue);
+    Serial.print("yValue: "); Serial.println(yValue);
+    Serial.print("opMode: "); Serial.println(opMode);
+    Serial.println("=====================");
+  
+    // // If you want to see raw bytes (optional)
+    // Serial.print("Raw bytes: ");
+    // for(byte i = 0; i < len; i++) {
+    //   Serial.print("0x");
+    //   Serial.print(rxBuf[i], HEX);
+    //   Serial.print(" ");
+    // }
+    // Serial.println();
+  }
 }
-
 
 void move_forward(int value) {
   Serial.println("MOVE FORWARD");
